f12cb2c90d358b64cbf0630439c7c55b1a26d71c
Maxime BOYER <boyer.m@sncf.fr>
2024-05-07 19:21:22 +0000

    bug : connection error

--- README.md
+++ README.md
@@ -1,18 +1,41 @@
 # Sentinel Hub API
 
-The main goal is to retrieve images from Sentinel-2 Level 2A using the [Sentinel Hub API](https://www.sentinel-hub.com/). We want to have a pyton script that requests the API in order to download the images that we want, given a location and a timeframe. 
+The main goal is to retrieve images from Sentinel-2 Level 2A using the [Sentinel Hub API](https://www.sentinel-hub.com/). We want to have a pyton script that requests the API in order to download the images that we want, given a location and a timeframe.
 
 **Constraints:**
+
 - Language: Python.
 - Expected file: Python file(s) or jupyter notebook (would prefer the former).
 - Function inputs: Coordinates for a **location**, as well as a **start dates** and **end dates**.
 - Expected output: Downloads all of the images for that location at those dates to a specific folder (cf the .env section).
 - The **location** will be given as a list of coordinates, and the **dates** will be inputed as strings in the format expected by the API, but it would be good to add failsafes incase the input isn't formatted the right way.
 
-
 **Allowed:**
+
 - Can import any and all necessary libraries.
 - Can assume that the user has a proper .env with the two following variables: the access token for the API, as well as the path to the folder for the images. Feel free to name both variables as you see fit, and to add more variables if needed.
 - Can assume that the user has the same versions of Python and libraries as you.
 
 That's all! Have a good day.
+
+**Problem with the connection**
+For now, I'm having a problem connecting to apihub.copernicus.eu through the api. I have registered and called the api using the correct username and password.
+
+With the vpn turned on, I get the error :
+
+<!-- language: none -->
+
+    requests.exceptions.ConnectionError: HTTPSConnectionPool(host='apihub.copernicus.eu', port=443): Max retries exceeded with url: /apihub/search?format=json&rows=100&start=0&q=beginPosition%3A%5B%222022-01-20T00%3A00%3A00Z%22+TO+%222022-03-20T00%3A00%3A00Z%22%5D+cloudcoverpercentage%3A%5B%220%22+TO+%2230%22%5D+platformname%3A%22Sentinel-2%22+footprint%3A%22Intersects%28POINT%2834.665+31.625%29%29%22 (Caused by NewConnectionError('<urllib3.connection.HTTPSConnection object at 0xffffb2923fd0>: Failed to establish a new connection: [Errno 111] Connection refused'))
+
+With the vpn turned off, I get the error :
+
+<!-- language: none -->
+
+    requests.exceptions.ConnectionError: ('Connection aborted.', ConnectionResetError(104, 'Connection reset by peer'))
+
+**Run**
+You can run the script using the command line :
+
+```console
+python3 /workspace/src/h2_startup/sentinel_api_call.py --location_lon 31.625 --location_lat 34.665 --start_date "2022-01-20" --end_date "2022-01-25"
+```
--- src/h2_startup/sentinel_api_call.py
+++ src/h2_startup/sentinel_api_call.py
@@ -1,32 +1,54 @@
-import json
 import os
-import time
-from typing import Dict, List, Optional, Tuple, Union
+from datetime import datetime
+from typing import Union
+from sentinelsat import SentinelAPI
+import click
 
-import numpy as np
-import pandas as pd
+def string_to_date(date_string: str) -> Union[datetime, None]:
+    try:
+        # Split the date string into year, month, and day components
+        year, month, day = map(int, date_string.split('-'))
+        # Create a datetime object
+        date_object = datetime(year, month, day).date()
+        return date_object
+    except ValueError:
+        # If the string format doesn't match, raise a DateFormatError
+        raise AssertionError("Input string does not match the expected format 'YYYY-MM-DD'")
 
+@click.command()
+@click.option('--location_lon', help='The longitude and longitude coordinates to retrieve images on.', type=float)
+@click.option('--location_lat', help='The longitude and longitude coordinates to retrieve images on.', type=float)
+@click.option('--start_date',
+              help="The start date to retrieve images from. Please provide it to the format 'YYYY-MM-DD'.", type=str)
+@click.option('--end_date',
+              help="The end date to retrieve images from. Please provide it to the format 'YYYY-MM-DD'.", type=str)
+def main(location_lon: float, location_lat:float, start_date: str, end_date: str) -> None:
 
-def main():
+    start_date = string_to_date(start_date)
+    end_date = string_to_date(end_date)
+    location = [location_lon, location_lat]
+    assert len(location) == 2, "Error with location parameter. Please provide longitude at index 0 and latitude at index 1"
+    assert isinstance(location[0], float) or isinstance(location[0], int), "Error with location parameter. Please provide longitude at index 0 of type float or int"
+    assert isinstance(location[1], float) or isinstance(location[1], int), "Error with location parameter. Please provide latitude at index 1 of type float or int"
+
     # connect to the API
-    from datetime import date
+    api = SentinelAPI(
+        os.getenv("ACCES_USER_ID"),
+        os.getenv("ACCES_PASSWORD"),
+        show_progressbars=True
+    )
 
-    from sentinelsat import SentinelAPI, geojson_to_wkt, read_geojson
+    # search by coordinates, time, and SciHub query keywords
+    products = api.query(
+        "POINT({0} {1})".format(location[0], location[1]),
+        date=(start_date, end_date),
+        platformname="Sentinel-2",
+        cloudcoverpercentage=(0, 30),
+    )
 
-    api = SentinelAPI('user', 'password', 'https://apihub.copernicus.eu/apihub')
-
-    # download single scene by known product id
-    api.download(<product_id>)
-
-    # search by polygon, time, and SciHub query keywords
-    footprint = geojson_to_wkt(read_geojson('/path/to/map.geojson'))
-    products = api.query(footprint,
-                        date=('20151219', date(2015, 12, 29)),
-                        platformname='Sentinel-2',
-                        cloudcoverpercentage=(0, 30))
-
+    print("Query done")
     # download all results from the search
-    api.download_all(products)
+    api.download_all(products, directory_path=os.getenv("DIR_DATA_PATH"))
 
     # convert to Pandas DataFrame
     products_df = api.to_dataframe(products)
@@ -37,12 +59,13 @@ def main():
     # GeoPandas GeoDataFrame with the metadata of the scenes and the footprints as geometries
     api.to_geodataframe(products)
 
-    # Get basic information about the product: its title, file size, MD5 sum, date, footprint and
-    # its download url
-    api.get_product_odata(<product_id>)
+    # # Get basic information about the product: its title, file size, MD5 sum, date, footprint and
+    # # its download url
+    # api.get_product_odata(<product_id>)
 
-    # Get the product's full metadata available on the server
-    api.get_product_odata(<product_id>, full=True)
+    # # Get the product's full metadata available on the server
+    # api.get_product_odata(<product_id>, full=True)
+
 
 if __name__ == "__main__":
     main()
